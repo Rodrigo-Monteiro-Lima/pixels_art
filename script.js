51e3d89f0c273240920f1708f2a3a2726172ac75
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-02 15:10:59 -0300

    Resolução do terceiro requisito

---
+++ script.js
@@ -0,0 +1,8 @@
+let blackColor = document.querySelector('.color');
+blackColor.style.backgroundColor = 'black';
+let redColor = document.querySelector('#color-palette').firstElementChild.nextElementSibling;
+redColor.style.backgroundColor = 'red';
+let blueColor = redColor.nextElementSibling;
+blueColor.style.backgroundColor = 'blue';
+let greenColor = blueColor.nextElementSibling;
+greenColor.style.backgroundColor = 'green';
