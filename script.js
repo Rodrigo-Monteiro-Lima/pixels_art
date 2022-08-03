cc2c6ad6b35ded63c15da598e60ac8b5caea7667
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-02 23:20:26 -0300

    Correção Lint 2

--- script.js
+++ script.js
@@ -1,97 +1,107 @@
-let blackColor = document.querySelector('.color');
+const blackColor = document.querySelector('.color');
 blackColor.style.backgroundColor = 'black';
-let redColor = document.querySelector('#color-palette').firstElementChild.nextElementSibling;
+const redColor = document.querySelector('#color-palette').firstElementChild.nextElementSibling;
 redColor.style.backgroundColor = 'red';
-let blueColor = redColor.nextElementSibling;
+const blueColor = redColor.nextElementSibling;
 blueColor.style.backgroundColor = 'blue';
-let greenColor = blueColor.nextElementSibling;
+const greenColor = blueColor.nextElementSibling;
 greenColor.style.backgroundColor = 'green';
 
-blackColor.addEventListener('click', addClass);
-redColor.addEventListener('click', addClass);
-blueColor.addEventListener('click', addClass);
-greenColor.addEventListener('click', addClass);
 function addClass(event) {
   blackColor.classList.remove('selected');
   redColor.classList.remove('selected');
   blueColor.classList.remove('selected');
-  greenColor.classList.remove('selected');   
+  greenColor.classList.remove('selected');
   event.target.classList.add('selected');
-};
+}
+blackColor.addEventListener('click', addClass);
+redColor.addEventListener('click', addClass);
+blueColor.addEventListener('click', addClass);
+greenColor.addEventListener('click', addClass);
 
-let board = document.querySelector('#pixel-board');
-let pixel = document.querySelector('.color');
-let colorSelected = document.querySelectorAll('.color');
+const board = document.querySelector('#pixel-board');
+const colorSelected = document.querySelectorAll('.color');
+const cN = 'color selected';
 board.addEventListener('click', (event) => {
+  const evt = event;
   for (let index = 0; index < colorSelected.length; index += 1) {
-    let pixelColor = colorSelected[index].style.backgroundColor;  
-    if (colorSelected[index].className === 'color selected' && event.target.style.color !== pixelColor && event.target.className === 'pixel') {
-      event.target.style.backgroundColor = pixelColor;         
+    const pC = colorSelected[index].style.backgroundColor;
+    const cs = colorSelected[index];
+    if (cs.className === cN && evt.target.className === 'pixel' && evt.target.style.color !== pC) {
+      evt.target.style.backgroundColor = pC;
     }
   }
 });
 
-let btnClear = document.querySelector('#clear-board');
-let pixelBox = document.querySelectorAll('.pixel');
-let primaryColor = 'white';
-//let input = document.querySelector('#board.size');
-btnClear.addEventListener('click', function() {
+const btnClear = document.querySelector('#clear-board');
+const pixelBox = document.querySelectorAll('.pixel');
+const primaryColor = 'white';
+btnClear.addEventListener('click', () => {
   for (let index = 0; index < pixelBox.length; index += 1) {
     if (pixelBox[index].style.backgroundColor !== primaryColor) {
       pixelBox[index].style.backgroundColor = primaryColor;
-    }  
-  }
-});
-
-let btnGenBoard = document.querySelector('#generate-board');
-btnGenBoard.addEventListener('click', function() {
-  let numOfPixel = document.getElementById('board-size').value;
-  let lines = document.querySelectorAll('.line');
-  let pixels = document.querySelectorAll('.pixel');
-
-  if (numOfPixel === '') {
-    alert('Board inválido!')
-  } else {
-    for (let index = 0; index < pixels.length; index += 1){
-      pixels[index].remove();
-    }
-    for (let index2 = 0; index2 < lines.length; index2 += 1) {
-      lines[index2].remove();
-    };
-
-    let num = parseInt(numOfPixel);
-    //console.log(num, typeof(num));
-    num = sizeLimits(num);      
-    for (let index3 = 0; index3 < num; index3 += 1) {
-      let divLine = document.createElement('div');
-      divLine.className = 'line';
-      board.appendChild(divLine);
-      for (let index4 = 0; index4 < num; index4 += 1) {
-        let divPixel = document.createElement('div');
-        divPixel.className = 'pixel'
-        divLine.appendChild(divPixel);
-      }    
     }
   }
 });
 
 function sizeLimits(num) {
+  const newNum = num;
+  let oldNum = num;
   if (num < 5) {
-    num = 5;
+    oldNum = 5;
   } else if (num > 50) {
-    num = 50;  
+    oldNum = 50;
   } else {
-    num = num;
+    oldNum = newNum;
   }
-  return num;
-};
+  return oldNum;
+}
+
+function removeBoard() {
+  const pixels = document.querySelectorAll('.pixel');
+  const lines = document.querySelectorAll('.line');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].remove();
+  }
+  for (let index2 = 0; index2 < lines.length; index2 += 1) {
+    lines[index2].remove();
+  }
+}
+
+function createBox(num) {
+  for (let index3 = 0; index3 < num; index3 += 1) {
+    const divLine = document.createElement('div');
+    divLine.className = 'line';
+    board.appendChild(divLine);
+    for (let index4 = 0; index4 < num; index4 += 1) {
+      const divPixel = document.createElement('div');
+      divPixel.className = 'pixel';
+      divLine.appendChild(divPixel);
+    }
+  }
+}
+
+const btnGenBoard = document.querySelector('#generate-board');
+btnGenBoard.addEventListener('click', () => {
+  const numOfPixel = document.getElementById('board-size').value;
+  if (numOfPixel === '') {
+    alert('Board inválido!');
+  } else {
+    removeBoard();
+    let num = parseInt(numOfPixel, 10);
+    num = sizeLimits(num);
+    createBox(num);
+  }
+});
 
 function generateColors() {
-  redColor.style.backgroundColor = 'rgb(' + Math.random()*255 + ',' + Math.random()*255 + ',' +Math.random()*255 + ')';
-  blueColor.style.backgroundColor = 'rgb(' + Math.random()*255 + ',' + Math.random()*255 + ',' +Math.random()*255 + ')';
-  greenColor.style.backgroundColor = 'rgb(' + Math.random()*255 + ',' + Math.random()*255 + ',' +Math.random()*255 + ')';
-  
+  redColor.style.backgroundColor = `rgb(${Math.random()
+    * 255},${Math.random() * 255},${Math.random() * 255})`;
+  blueColor.style.backgroundColor = `rgb(${Math.random()
+    * 255},${Math.random() * 255},${Math.random() * 255})`;
+  greenColor.style.backgroundColor = `rgb(${Math.random()
+    * 255},${Math.random() * 255},${Math.random() * 255})`;
 }
-window.onload = function() {
+window.onload = function initial() {
   generateColors();
 };
